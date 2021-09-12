c645501d45de1a2e29f8875383af8242dd281285
Junan02 <87476149+Junan02@users.noreply.github.com>
2021-09-12 13:15:50 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,15 @@ if [[ -n $RCLONE_CONFIG ]]; then
  echo -e "$RCLONE_CONFIG" > /app/rclone.conf
 fi
 python3 -m tgtlg
+
+if [[ -n $TOKEN_PICKLE_URL ]]; then
+	wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
+fi
+
+if [[ -n $ACCOUNTS_ZIP_URL ]]; then
+	wget -q $ACCOUNTS_ZIP_URL -O /usr/src/app/accounts.zip
+	unzip accounts.zip -d /usr/src/app/accounts
+	rm accounts.zip
+fi
+
+./aria.sh; python3 -m tgtlg
